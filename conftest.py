94292e6ef104c6ae98a09a42b6a2ddd4d7758eab
Maxim Shavel <maksimka411@yandex.ru>
2025-04-16 03:33:16 +0300

    New_commit

--- conftest.py
+++ conftest.py
@@ -5,18 +5,3 @@ from main import BooksCollector
 def collector():
     return BooksCollector()
 
-@pytest.fixture
-def add_book(collector):
-    return collector.add_new_book
-
-@pytest.fixture
-def add_genre(collector):
-    return collector.set_book_genre
-
-@pytest.fixture
-def add_to_favorites(collector):
-    return collector.add_book_in_favorites
-
-@pytest.fixture
-def delete_from_favorites(collector):
-    return collector.delete_book_from_favorites
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import pytest
-from conftest import collector, add_book, add_genre, add_to_favorites, delete_from_favorites
+from conftest import collector
 from main import (BooksCollector)
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -26,37 +26,49 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
 
-    def test_add_new_book_add_three_books(self, collector, add_book):
-        add_book('Азазель')
-        add_book('Укрытие')
-        add_book('Сияние')
+    def test_add_new_book_add_three_books(self, collector):
+        collector.add_new_book('Азазель')
+        collector.add_new_book('Укрытие')
+        collector.add_new_book('Сияние')
         assert len(collector.get_books_genre()) == 3
 
-    def test_add_new_book_add_duplicate(self, collector, add_book):
-        add_book('Дракула')
-        add_book('Дракула')
+    def test_add_new_book_add_duplicate(self, collector):
+        collector.add_new_book('Дракула')
+        collector.add_new_book('Дракула')
         assert len(collector.get_books_genre()) == 1
 
-    def test_add_new_book_incorrect_length(self, collector, add_book):
-        add_book('')
-        add_book('ЖизньиудивительныеприключенияРобинзонаКру')
+    def test_add_new_book_incorrect_length(self, collector):
+        collector.add_new_book('')
+        collector.add_new_book('ЖизньиудивительныеприключенияРобинзонаКру')
         assert len(collector.get_books_genre()) == 0
 
-    def test_set_book_genre_correct(self, collector, add_book, add_genre):
-        add_book('Чемодан')
-        add_genre('Чемодан', 'Комедии')
+    def test_set_book_genre_correct(self, collector):
+        collector.add_new_book('Чемодан')
+        collector.set_book_genre('Чемодан', 'Комедии')
         assert collector.get_book_genre('Чемодан') == 'Комедии'
 
-    def test_set_book_genre_incorrect(self, collector, add_book, add_genre):
-        add_book('Три товарища')
-        add_genre('Три товарища', 'Роман')
+    def test_set_book_genre_incorrect(self, collector):
+        collector.add_new_book('Три товарища')
+        collector.set_book_genre('Три товарища', 'Роман')
         assert collector.get_book_genre('Три товарища') == ''
 
-    @pytest.mark.parametrize('book_name, genre', [('Азазель', 'Детективы'), ('Дракула', 'Ужасы')])
-    def test_get_books_with_specific_genre(self, collector, add_book, add_genre, book_name, genre):
-        add_book(book_name)
-        add_genre(book_name, genre)
-        assert collector.get_books_with_specific_genre(genre) == [book_name]
+    def test_get_book_genre_book_without_genre(self, collector):
+        collector.add_new_book('Три товарища')
+        assert collector.get_book_genre('Три товарища') == f''
+
+
+    def test_get_books_with_specific_genre_invalid_genre(self, collector):
+        collector.add_new_book('Чемодан')
+        collector.set_book_genre('Чемодан', 'Комедии')
+        assert collector.get_books_with_specific_genre('Драмма') == []
+
+
+    def test_get_books_with_specific_genre(self, collector):
+        collector.add_new_book('Азазель')
+        collector.set_book_genre('Азазель', 'Детективы')
+        collector.add_new_book('Шерлок Холмс')
+        collector.set_book_genre('Шерлок Холмс', 'Детективы')
+        assert collector.get_books_with_specific_genre('Детективы') == ['Азазель', 'Шерлок Холмс']
 
     def test_get_books_with_specific_genre_empty(self, collector):
         assert collector.get_books_with_specific_genre('Фантастика') == []
@@ -88,6 +100,15 @@ class TestBooksCollector:
         delete_from_favorites('Шерлок Холмс')
         assert 'Шерлок Холмс' not in collector.get_list_of_favorites_books()
 
+
+
     def test_get_list_of_favorites_books_empty(self, collector):
         assert collector.get_list_of_favorites_books() == []
-lector()
+
+    def test_get_list_of_favorites_books(self, collector):
+        collector.add_new_book('Туман')
+        collector.add_book_in_favorites('Туман')
+        assert collector.get_list_of_favorites_books() == ['Туман']
+
+
+
